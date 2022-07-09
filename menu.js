5fb6c9aa1418a18db028fc2c546616ebe1f63ca9
Rekojammy <rejoiceuchechukwu@gmail.com>
2022-07-09 18:33:51 +0100

    second commit

---
+++ menu.js
@@ -0,0 +1,8 @@
+const menu = document.getElementById('menu');
+const smNav = document.getElementById('smNav');
+
+
+
+menu.addEventListener('click', () => {
+    smNav.classList.toggle('hidden')
+})
